1cd0ee5857ae88819efea0102a31f65d2ad58533
Leron <lc443@njit.edu>
2022-10-15 20:20:49 -0400

    adding content

--- user-service/src/main/java/com/leron/user/controller/UserController.java
+++ user-service/src/main/java/com/leron/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.leron.user.controller;
 
 import com.leron.user.model.User;
 import com.leron.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -16,16 +18,19 @@ public class UserController {
 
     @PostMapping("/")
     public User save( @RequestBody User user) {
+        log.info("Inside save user of user controller");
         return userService.save(user);
     }
 
     @GetMapping("/{id}")
     public User findUserById(@PathVariable("id") Long id) {
+        log.info("Inside find by Id of user controller");
         return userService.findUserById(id);
     }
 
     @GetMapping
     public List<User> findAllUsers() {
+        log.info("Inside find all users of controller");
         return userService.findAllUsers();
     }
 
--- user-service/src/main/java/com/leron/user/service/UserService.java
+++ user-service/src/main/java/com/leron/user/service/UserService.java
@@ -26,5 +26,4 @@ public class UserService {
         return userRepository.findAll();
     }
 
-
 }
